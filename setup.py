88964496d274483e1a13efc3c2f381dc892b0f58
jakhmoladp <54409180+jakhmoladp@users.noreply.github.com>
2022-10-12 08:49:10 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python_project_structure',
+    url='https://github.com/jakhmoladp/git_project_structure.git',
+    author='Devendra Prasad Jakhmola',
+    author_email='devjakhmola1990@gmail.com',
+    packages=['python_project_structure'],
+    install_requires=['numpy', 'pandas', 'streamlit','sklearn','joblib'],
+    version='0.1',
+    license='MIT',
+    description='An example of how a basic project structure would look like'
+)

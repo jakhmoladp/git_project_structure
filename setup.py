86d91c98b8ffa4f555a1d2eb4d2344f83ed74319
Rashmi Raturi <rraturi@adobe.com>
2022-10-12 09:24:22 +0530

    tree file added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/jakhmoladp/git_project_structure.git',
     author='Devendra Prasad Jakhmola',
     author_email='devjakhmola1990@gmail.com',
-    packages=['python_project_structure'],
+    packages=find_package(),
     install_requires=['numpy', 'pandas', 'streamlit','sklearn','joblib'],
     version='0.1',
     license='MIT',
